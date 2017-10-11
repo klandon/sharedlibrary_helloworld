6045ead577b4eff93ea3a32b426c1d783c1a63bc
Landon <landonkt@C02T7BAWGTFM.legal.regn.net>
2017-10-11 06:45:04 -0400

    fix typo

--- pipeline/test_pipline.groovy
+++ pipeline/test_pipline.groovy
@@ -1,7 +1,7 @@
 #!groovy
 @Library('helloworld@master') _
 
-def hw = new org.helloworld.test.Hello();
+def hw = new org.helloworld.test.Hello("some message");
 
 pipeline
 {
--- src/org/helloworld/test/Hello.groovy
+++ src/org/helloworld/test/Hello.groovy
@@ -4,10 +4,6 @@ class SayHello implements Serializable
 {
   String messageToSay = "Default Hello World Message";
 
-  Hello()
-  {
-
-  }
 
   Hello(string newMessage)
   {
